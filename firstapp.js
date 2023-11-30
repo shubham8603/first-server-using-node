72049664b68b001139b2a97da8311a7aec3f1935
shubham8603 <shubham8603@gmail.com>
2023-11-30 22:26:00 +0530

    added custom response for url

--- firstapp.js
+++ firstapp.js
@@ -1,9 +1,24 @@
 const http = require('http');
 
-
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    console.log(url, req.method, req.headers);
+    //process.exit()
+    res.setHeader('content-type', 'text/html');
+
+    if (url === '/home') {
+        res.write('<html><body><h1>Welcome home</h1></body></html>');
+    } else if (url === '/about') {
+        res.write('<html><body><h1>Welcome to About Us page</h1></body></html>');
+    } else if (url === '/node') {
+        res.write('<html><body><h1>Welcome to my Node Js project</h1></body></html>');
+    } else {
+        res.write('<html><body><h1>Page not found</h1></body></html>');
+    }
+
+    res.end();
 });
+
 server.listen(4000, () => {
-    console.log('shubham')
+    console.log('Server listening on port 4000');
 });
