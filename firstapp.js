a3c6c16ea46693acc720221af132d1a1d2dc9243
shubham8603 <shubham8603@gmail.com>
2024-01-06 02:41:35 +0530

    body-parser

--- firstapp.js
+++ firstapp.js
@@ -1,33 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the 1st middleware");
-    next(); // Allow the request to continue to the next middleware in the line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res) => {
+    console.log("In the middleware");
+    res.send('<form action="/product" method="POST">' +
+             '<input type="text" name="productName" placeholder="Product Name"><br>' +
+             '<input type="number" name="productsize" placeholder="Product Size"><br>' +
+             '<button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("In the 2nd middleware");
-    res.send('<h1>Hello world</h1>');
+app.use('/product', (req, res) => {
+    console.log("Product Name:", req.body.productName);
+    console.log("Product Size:", req.body.productsize);
+    res.redirect('/');
 });
 
-app.post('/submit', (req, res) => {
-    // Handle the /submit route for POST requests
-    res.send('Handling submit route for POST request');
+app.use('/', (req, res) => {
+    console.log("In the next middleware");
+    res.send('<h1>Hello from express</h1>');
 });
 
-app.post('/about', (req, res) => {
-    // Handle the /about route for POST requests
-    res.send('Handling about route for POST request');
-});
-
-app.use((req, res) => {
-    // Handle default route
-    res.send('Handling default route');
-});
-
-const port = 4000;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+app.listen(4000);
