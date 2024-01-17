c5c7610832d395dfc5b12464765377cc2c8fe6ec
shubham8603 <shubham8603@gmail.com>
2024-01-17 10:37:33 +0530

    router and filters

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 router.get('/add-product', (req, res,next) => {
    console.log('hi');
-    res.send('<form action="add-product" method="POST">' +
+    res.send('<form action="/admin/add-product" method="POST">' +
              '<input type="text" name="productName" placeholder="Product Name"><br>' +
              '<input type="number" name="productsize" placeholder="Product Size"><br>' +
              '<button type="submit">Add Product</button></form>');
 });
-router.post('/product',(req, res, next) => {
+router.post('/add-product',(req, res, next) => {
     console.log("hello");
         console.log(req.body);
         res.redirect('/');
