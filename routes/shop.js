3b9e59c63cd242fbb19e32743d6e88f9b29d962d
shubham8603 <shubham8603@gmail.com>
2024-01-19 07:40:01 +0530

    serving html

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from shop</h1>');
-    console.log('shop');
+    res.sendFile(path.join(__dirname,'../','views','shop.html'));
 });
 
 module.exports = router;
